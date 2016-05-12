e72538893adf48aa19813100b7ae556429aa12bb
Nobodies <Femida1990@mail.ru>
2016-05-12 16:47:14 +0300

    Template

---
+++ README.md
@@ -0,0 +1,11 @@
+Чтобы развернуть проект необходимо установить:
+
+NodeJS
+Bower
+Для устновки зависимостей и зпуска сервера выполните последовтельно:
+
+1.git clone https://github.com/Nob0dies/mySite.git mySite
+2.cd mySite
+3.bower i
+4.npm i
+5.gulp
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Nobodies on 12.05.2016.
+ */
+var gulp = require("gulp"),
+    browserSync = require('browser-sync'),
+    jade = require('gulp-jade'),
+    sass = require('gulp-sass');
+
+gulp.task('jade', function() {
+    gulp.src('app/jade/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./app/'));
+});
+
+
+//компилятор sass файлов
+gulp.task('sass', function () {
+    gulp.src('app/sass/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('app/css'));
+});
+
+gulp.task("server",function(){
+    browserSync({
+        port:9000,
+        server:{
+            baseDir:"app"
+        }
+    });
+
+});
+gulp.task('watch', function () {
+    gulp.watch('app/jade/**/*', ['jade']);
+    gulp.watch('app/sass/**/*', ['sass']);
+    gulp.watch([
+        'app/index.html',
+        'app/*.html',
+        'app/js/**/*.js',
+        'app/css/**/*.css'
+    ]).on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['server', 'watch']);
